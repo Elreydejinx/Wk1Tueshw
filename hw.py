fe7c8b5dadc61f4c0ada558ad9144fce8646ff18
Jeremiah-Williams <jae4lani17@gmail.com>
2024-06-11 19:00:25 -0400

    hy.py

--- hw.py
+++ hw.py
@@ -2,7 +2,7 @@
 
 # Buggy Code:
 
-number = input("Enter a number:")
+number = (1 - 3)
 
 if number > 0:
     print("The number is positive.")
@@ -10,3 +10,17 @@ elif number == 0:
     print("The number is zero.")
 else: 
     print("The number is negative.")
+
+
+# The Greatest Showdown
+
+Number = 12
+
+if Number <= 4:
+    print("Got a low baller here")
+elif Number <= 8:
+    print("now we're talking")
+elif Number >= 12:
+    print("Well don't that beat all")
+else:
+    print("now your not paying attention to the rules")
